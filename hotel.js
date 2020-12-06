9c0366c8c3e61edf69440be6d09334483d0528bf
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-12-06 07:44:25 +0300

    complete pages

---
+++ hotel.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    $("#addCommentBtn").click(function(){
+      var commentRow = `<div class="yorumRow">
+                        <div class="yorum">
+                            <div class="userPicContainer">
+                                <img class="userPic" src="images/Layer6.png">
+                            </div>
+                            <div class="yorumTextName">
+                                <p class="yorumText">
+                                    "Kışın zorlu şartlarda kaldığımız bu otelimizde
+                                    inanılmaz keyifli zamanlar geçirdik. Kış uykusuna
+                                    yatan ayıları mağarada izleme fırsatı yakaladık."
+                                </p>
+                            
+                                <h3>EMILY GWENDSONN</h3>
+                            </div>
+                        </div>
+
+                        <div class="yorum">
+                            <div class="userPicContainer">
+                                <img class="userPic" src="images/Layer8.png">
+                            </div>
+                            <div class="yorumTextName">
+                                <p class="yorumText">
+                                    "Geyikleri avlayamasak da onları takip etmek, onların hayatlarının
+                                    birer parçası olmak inanılmazdı. Otel rehberimiz Mr. Joe Biden'a
+                                    güzel yönlendirmeleri için teşekkür ederim."
+                                </p>
+
+                                <h3>ROBERT JULIAN</h3>
+                            </div>
+                        </div>
+                    </div>`
+      $("#yorumlarBody").append(commentRow)
+      alert("Success!");
+    });
+
+    $("#gönderBtn").click(function(){
+
+        var date = new Date();
+        var day = date.getDate();
+
+        if($('#arrivalDate').val().slice(8, 10) < day || $('#leaveDate').val().slice(8, 10) <= day){
+            alert("Please pick proper date")
+        }
+
+        if(!$('#fname').val() || 
+           !$('#arrivalDate').val() || 
+           !$('#email').val() || 
+           !$('#leaveDate').val() || 
+           !$('#phone').val()){
+            alert("Please fill all the fields")
+        }
+    });
+});
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("harita"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
